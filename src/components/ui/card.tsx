5ed2973c397a00f250cde851a88c6429c3ae98c9
builderfahimhub <houkprothibad12@gmail.com>
2026-04-15 03:58:04 +0600

    Create card.tsx
    Signed-off-by: builderfahimhub <houkprothibad12@gmail.com>

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,11 @@
+export const Card = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export const CardContent = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export const CardHeader = ({ children }) => {
+  return <div>{children}</div>;
+};
