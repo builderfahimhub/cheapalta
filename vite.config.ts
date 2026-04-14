ac76dd4d725dbb622d8cfecf26cebc19ce996206
builderfahimhub <houkprothibad12@gmail.com>
2026-04-15 04:46:03 +0600

    Update vite.config.ts
    Signed-off-by: builderfahimhub <houkprothibad12@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],   // IMPORTANT: comma must be here
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 
   define: {
     global: 'window'
